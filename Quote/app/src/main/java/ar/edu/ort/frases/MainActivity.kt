771ea674ea9691bf2c6f940149af33f3ff4a74e9
Martin Rivas <dibuyo@gmail.com>
2024-10-28 11:32:30 -0300

    Update Quote

--- Quote/app/src/main/java/ar/edu/ort/frases/MainActivity.kt
+++ Quote/app/src/main/java/ar/edu/ort/frases/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -44,7 +45,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(quote: String,  author: String, modifier: Modifier = Modifier) {
-    Row() {
+    Column() {
         Text(
             text = quote,
             modifier = modifier
