7d6722d0cd6bd63d3f12d3a19df68a690e8dbd9c
Martin Rivas <dibuyo@gmail.com>
2025-04-21 09:33:01 -0300

    Update Challenges

--- Challenges/First/app/src/main/java/ar/edu/ort/challenge/first/screens/login/LoginScreen.kt
+++ Challenges/First/app/src/main/java/ar/edu/ort/challenge/first/screens/login/LoginScreen.kt
@@ -1,25 +1,35 @@
 package ar.edu.ort.challenge.first.screens.login
 
+import android.renderscript.ScriptGroup.Input
 import ar.edu.ort.challenge.first.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.DisplayMode.Companion.Input
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
 
 @Composable
 fun LoginScreen(modifier: Modifier){
-    Column(
+    Row(){
+        Text("Login here")
+        Text("Welcome back you’ve\n" +
+                "been missed!")
+
+    }
+    /*Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Alignment.CenterVertically as Arrangement.Vertical,
+        //verticalArrangement = Alignment.CenterVertically as Arrangement.Vertical,
     ){
         Box(modifier = Modifier.fillMaxWidth()){
             Column {
@@ -31,5 +41,11 @@ fun LoginScreen(modifier: Modifier){
                 )
             }
         }
-    }
+    }*/
+}
+
+@Composable
+@Preview(showBackground = true)
+fun LoginScreenPreview() {
+    LoginScreen(Modifier)
 }
